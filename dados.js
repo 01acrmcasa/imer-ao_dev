bd45e41115b807752f37397bd68b253c71cf7648
01acrmcasa <macedoclara096@gmail.com>
2024-09-07 19:31:58 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let Oppenheimer = {
+    titulo: "2024: Oppenheimer",
+    descricao: "Um drama biográfico que nos leva aos bastidores da criação da bomba atômica e explora o dilema moral do físico J. Robert Oppenheimer.",
+    link: "https://www.adorocinema.com/filmes/filme-296168/",
+    tags: ["biografia", "bomba atomica", "fisico", "2024", "filme", "oscar"]
+};
+let Tudo_Em_Todo_Lugar_Ao_Mesmo_Tempo = {
+    titulo: "2023: Tudo em Todo Lugar ao Mesmo Tempo",
+    descricao: "Uma comédia maluca e emocionante que explora os multiversos e nos apresenta uma família bem peculiar.",
+    link: "https://www.adorocinema.com/filmes/filme-270743/",
+    tags: ["comedia", "familia", "multiversos", "2023", "filme", "oscar"]
+};
+
+let No_Ritmo_Do_Coraçao = {
+    titulo: "2022: No Ritmo do Coração",
+    descricao: "Um filme inspirador sobre a vida de um jovem surdo que busca realizar seu sonho de se tornar um baterista de jazz.",
+    link: "https://www.adorocinema.com/filmes/filme-276170/",
+    tags: ["baterista", "jazz", "inspirador", "2022", "filme", "oscar"]
+};
+
+let Nomadland = {
+    titulo: "2021: Nomadland",
+    descricao: "Um drama emocionante que acompanha uma mulher que decide viver em uma van e viajar pelos Estados Unidos após perder tudo.",
+    link: "https://www.adorocinema.com/filmes/filme-271687/",
+    tags: ["viajar", "mulher", "van", "2021", "filme", "oscar"]
+};
+
